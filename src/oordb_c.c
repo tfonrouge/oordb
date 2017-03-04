1d7e7f3fa598a7b75db959023b277e172411ea40
Teo Fonrouge <tfonrouge@gmail.com>
2017-03-03 20:23:46 -0600

    * moved c code to c source

---
+++ src/oordb_c.c
@@ -0,0 +1,26 @@
+/*
+ *
+ */
+
+#include "hbapi.h"
+#include "hbapiitm.h"
+#include "hbstack.h"
+
+HARBOUR HB_FUNC_EXEC( HB_HAUTOADD );
+HARBOUR HB_FUNC_EXEC( HB_HCASEMATCH );
+HARBOUR HB_FUNC_EXEC( HB_HBINARY );
+HARBOUR HB_FUNC_EXEC( HB_HKEEPORDER );
+
+HB_FUNC( OORDBBASE_OBJECTH )
+{
+  PHB_ITEM pSelf = hb_stackSelfItem();
+  if( pSelf )
+  {
+    hb_retptr( hb_arrayId( pSelf ) );
+  }
+}
+
+HB_FUNC( HB_HSETAUTOADD )     { HB_FUNC_EXEC( HB_HAUTOADD ); hb_itemReturn( hb_param( 1, HB_IT_HASH ) ); }
+HB_FUNC( HB_HSETCASEMATCH )   { HB_FUNC_EXEC( HB_HCASEMATCH ); hb_itemReturn( hb_param( 1, HB_IT_HASH ) ); }
+HB_FUNC( HB_HSETBINARY )      { HB_FUNC_EXEC( HB_HBINARY ); hb_itemReturn( hb_param( 1, HB_IT_HASH ) ); }
+HB_FUNC( HB_HSETORDER )       { HB_FUNC_EXEC( HB_HKEEPORDER ); hb_itemReturn( hb_param( 1, HB_IT_HASH ) ); }
