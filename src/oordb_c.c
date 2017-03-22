93276b5648fa0fff66feb86be1ed0cd0aa482fd9
Teo Fonrouge <tfonrouge@gmail.com>
2017-03-22 17:52:58 -0600

    * conditional compiling for Harbour 3.4

--- src/oordb_c.c
+++ src/oordb_c.c
@@ -6,10 +6,17 @@
 #include "hbapiitm.h"
 #include "hbstack.h"
 
-HARBOUR HB_FUNC_EXEC( HB_HAUTOADD );
-HARBOUR HB_FUNC_EXEC( HB_HCASEMATCH );
-HARBOUR HB_FUNC_EXEC( HB_HBINARY );
-HARBOUR HB_FUNC_EXEC( HB_HKEEPORDER );
+#if HB_VER_MAJOR > 3 || (HB_VER_MAJOR == 3 && HB_VER_MINOR >= 4)
+    HARBOUR HB_FUNC_EXEC( HB_HAUTOADD );
+    HARBOUR HB_FUNC_EXEC( HB_HCASEMATCH );
+    HARBOUR HB_FUNC_EXEC( HB_HBINARY );
+    HARBOUR HB_FUNC_EXEC( HB_HKEEPORDER );
+
+    HB_FUNC( HB_HSETAUTOADD )     { HB_FUNC_EXEC( HB_HAUTOADD ); hb_itemReturn( hb_param( 1, HB_IT_HASH ) ); }
+    HB_FUNC( HB_HSETCASEMATCH )   { HB_FUNC_EXEC( HB_HCASEMATCH ); hb_itemReturn( hb_param( 1, HB_IT_HASH ) ); }
+    HB_FUNC( HB_HSETBINARY )      { HB_FUNC_EXEC( HB_HBINARY ); hb_itemReturn( hb_param( 1, HB_IT_HASH ) ); }
+    HB_FUNC( HB_HSETORDER )       { HB_FUNC_EXEC( HB_HKEEPORDER ); hb_itemReturn( hb_param( 1, HB_IT_HASH ) ); }
+#endif
 
 HB_FUNC( OORDBBASE_OBJECTH )
 {
@@ -19,8 +26,3 @@ HB_FUNC( OORDBBASE_OBJECTH )
     hb_retptr( hb_arrayId( pSelf ) );
   }
 }
-
-HB_FUNC( HB_HSETAUTOADD )     { HB_FUNC_EXEC( HB_HAUTOADD ); hb_itemReturn( hb_param( 1, HB_IT_HASH ) ); }
-HB_FUNC( HB_HSETCASEMATCH )   { HB_FUNC_EXEC( HB_HCASEMATCH ); hb_itemReturn( hb_param( 1, HB_IT_HASH ) ); }
-HB_FUNC( HB_HSETBINARY )      { HB_FUNC_EXEC( HB_HBINARY ); hb_itemReturn( hb_param( 1, HB_IT_HASH ) ); }
-HB_FUNC( HB_HSETORDER )       { HB_FUNC_EXEC( HB_HKEEPORDER ); hb_itemReturn( hb_param( 1, HB_IT_HASH ) ); }
